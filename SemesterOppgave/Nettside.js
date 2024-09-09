55237e8e93a43c88f6329a6c9f5fdf449ae46856
LightyMatter <felixtvete@gmail.com>
2024-09-09 15:23:19 +0200

    Semesteroppg
    Nettside.htm er hjemsiden.

---
+++ SemesterOppgave/Nettside.js
@@ -0,0 +1,10 @@
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offScreenMenu.classList.toggle('active');
+})
+
+const Home = document.querySelector('mBarHo');
